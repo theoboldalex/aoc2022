23f50189951b1af829255b0d88df5568e0b0c079
alex.theobold <alex.theobold@sykescottages.co.uk>
2022-12-02 17:06:31 +0000

    Two Turtle Doves

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -2,12 +2,52 @@ import os
 import requests
 from dotenv import load_dotenv
 
+def process_data(data: str) -> list:
+    return [x.split() for x in data.strip().split("\n")]
+
+def convert_to_base_score(letter: str) -> int:
+    if letter in ["A", "X"]:
+        return 1
+    if letter in ["B", "Y"]:
+        return 2
+    if letter in ["C", "Z"]:
+        return 3
+
+def get_scores_for_round(scores: list) -> list:
+   return [convert_to_base_score(score) for score in scores]
+
+def get_round_winner(scores: list) -> int:
+    # case for a draw - quick and dirty
+    if scores[0] == scores[1]:
+        return 2
+    if scores in [[1, 2], [2, 3], [3, 1]]:
+        return 1
+    return 0
+
+def set_round_scores(round: list) -> list:
+    round_winner = get_round_winner(round)
+    if round_winner < 2:
+        round[round_winner] += 6
+        return round
+    return [i + 3 for i in round]
+
+def transform_input_data(data: list) -> list:
+    return [get_scores_for_round(i) for i in data]
+
+def get_totals(scores: list) -> list:
+    return [set_round_scores(i) for i in scores]
+
+def get_overall_winner_score(scores: list) -> int:
+    return sum([score[1] for score in scores])
+
 def main():
     load_dotenv()
     cookies = {"session": os.environ["COOKIE"]}
     r = requests.get("https://adventofcode.com/2022/day/2/input", cookies=cookies)
-    print(f"part 1: {}")
-    print(f"part 2: {}")
+    input_data = transform_input_data(process_data(r.text))
+    totals = get_totals(input_data)
+    print(f"part 1: {get_overall_winner_score(totals)}")
+    # print(f"part 2: {}")
 
 if __name__ == "__main__":
     main()
--- day_2/test_day_2.py
+++ day_2/test_day_2.py
@@ -1,8 +1,68 @@
-from day_2 import process_data
+from day_2 import (
+    process_data,
+    convert_to_base_score,
+    get_scores_for_round, 
+    get_round_winner, 
+    set_round_scores, 
+    transform_input_data,
+    get_totals,
+    get_overall_winner_score
+    )
 
 sample_data = '''
-
+A Y
+B X
+C Z
 '''
 
-def test_1():
-    pass
+def get_test_data(data: str) -> list:
+    return process_data(data)
+
+def test_process_data():
+    assert get_test_data(sample_data) == [['A', 'Y'], ['B', 'X'], ['C', 'Z']]
+
+def test_convert_to_base_score():
+    test_data = get_test_data(sample_data)
+    a = test_data[0][0]
+    b = test_data[1][0]
+    c = test_data[2][0]
+    x = test_data[1][1]
+    y = test_data[0][1]
+    z = test_data[2][1]
+    assert convert_to_base_score(a) == 1
+    assert convert_to_base_score(b) == 2
+    assert convert_to_base_score(c) == 3
+    assert convert_to_base_score(x) == 1
+    assert convert_to_base_score(y) == 2
+    assert convert_to_base_score(z) == 3
+
+def test_calculate_round_winner():
+    test_data = get_test_data(sample_data)
+    round_one = get_scores_for_round(test_data[0])
+    round_two = get_scores_for_round(test_data[1])
+    round_three = get_scores_for_round(test_data[2])
+    assert get_round_winner(round_one) == round_one.index(round_one[1])
+    assert get_round_winner(round_two) == round_two.index(round_two[0])
+    assert get_round_winner(round_three) == 2
+
+def test_update_scores():
+    test_data = get_test_data(sample_data)
+    round_one = get_scores_for_round(test_data[0])
+    round_two = get_scores_for_round(test_data[1])
+    round_three = get_scores_for_round(test_data[2])
+    assert set_round_scores(round_one) == [1, 8]
+    assert set_round_scores(round_two) == [8, 1]
+    assert set_round_scores(round_three) == [6, 6]
+
+def test_transform_input_data():
+    test_data = get_test_data(sample_data)
+    processed_rounds = transform_input_data(test_data)
+    processed_rounds = get_totals(processed_rounds)
+    assert processed_rounds == [[1, 8], [8, 1], [6, 6]]
+
+def test_get_overall_winner_score():
+    test_data = get_test_data(sample_data)
+    processed_rounds = transform_input_data(test_data)
+    processed_rounds = get_totals(processed_rounds)
+    assert get_overall_winner_score(processed_rounds) == 15
+
