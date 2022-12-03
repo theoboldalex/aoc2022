5abd2ab56e88de61118ca2d8387aae6a8f603691
theoboldalex <theoboldalex@gmail.com>
2022-12-03 20:27:06 +0000

    Three French Hens

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -2,7 +2,7 @@ import os
 import requests
 from dotenv import load_dotenv
 
-def process_data(sample: str) -> list:
+def process_part_one(sample: str) -> list:
     result = []
     items = [s for s in sample.strip().split("\n")]
     for i in items:
@@ -11,8 +11,18 @@ def process_data(sample: str) -> list:
 
     return result
 
+def process_part_two(sample: str) -> list:
+    result = []
+    items = [s for s in sample.strip().split("\n")]
+    for i in range(0, len(items), 3):
+        result.append(items[i:i + 3])
+    return result
+    
 def get_dupe_item(bag: list) -> str:
     return [item for item in bag[0] if item in bag[1]][0]
+
+def get_common_item(group: list) -> str:
+    return [item for item in group[0] if item in group[1] and item in group[2]][0]
 
 def encode_char_to_int(char: str) -> int:
     if ord(char) >= 97:
@@ -22,8 +32,10 @@ def encode_char_to_int(char: str) -> int:
 def main():
     load_dotenv()
     r = requests.get("https://adventofcode.com/2022/day/3/input", cookies={"session": os.environ["COOKIE"]})
-    data = process_data(r.text)
-    print(f"part 1: {sum([encode_char_to_int(get_dupe_item(i)) for i in data])}")
+    part_one = process_part_one(r.text)
+    part_two = process_part_two(r.text)
+    print(f"part 1: {sum([encode_char_to_int(get_dupe_item(i)) for i in part_one])}")
+    print(f"part 2: {sum([encode_char_to_int(get_common_item(i)) for i in part_two])}")
     # read from input file
     # get the dupe for each bag
     # encode them all
--- day_3/test_day_3.py
+++ day_3/test_day_3.py
@@ -1,5 +1,6 @@
 from day_3 import (  
-    process_data,
+    process_part_one,
+    process_part_two,
     get_dupe_item,
     encode_char_to_int
 )
@@ -13,11 +14,14 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
 '''
 
-def get_test_data() -> list:
-    return process_data(sample_data)
+def get_part_one_data() -> list:
+    return process_part_one(sample_data)
 
-def test_process_data():
-    assert get_test_data() == [
+def get_part_two_data() -> list:
+    return process_part_two(sample_data)
+
+def test_process_part_one():
+    assert get_part_one_data() == [
         ['vJrwpWtwJgWr', 'hcsFMMfFFhFp'],
         ['jqHRNqRjqzjGDLGL', 'rsFMfFZSrLrFZsSL'],
         ['PmmdzqPrV', 'vPwwTWBwg'],
@@ -26,11 +30,20 @@ def test_process_data():
         ['CrZsJsPPZsGz', 'wwsLwLmpwMDw'],
     ]
 
+def test_process_part_two():
+    assert get_part_two_data() == [
+        ['vJrwpWtwJgWrhcsFMMfFFhFp', 'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL', 'PmmdzqPrVvPwwTWBwg'],
+        ['wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn', 'ttgJtRGJQctTZtZT', 'CrZsJsPPZsGzwwsLwLmpwMDw'],
+    ]
+
 def test_get_dupe_item():
-    data = get_test_data()
+    data = get_part_one_data()
     assert get_dupe_item(data[0]) == 'p'
 
 def test_encode_char_to_int():
-    data = get_test_data()
+    data = get_part_one_data()
     char = get_dupe_item(data[0])
     assert encode_char_to_int(char) == 16 # "p".charCode() - 96
+
+def test_part_two():
+    pass
