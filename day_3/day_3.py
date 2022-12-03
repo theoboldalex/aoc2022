fb9274387d5bffb6785a2fd3d1c5a79c778c8c3b
theoboldalex <theoboldalex@gmail.com>
2022-12-03 18:36:12 +0000

    Three French Hens

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,3 +1,7 @@
+import os
+import requests
+from dotenv import load_dotenv
+
 def process_data(sample: str) -> list:
     result = []
     items = [s for s in sample.strip().split("\n")]
@@ -6,3 +10,24 @@ def process_data(sample: str) -> list:
         result.append([first_compartment, second_compartement])
 
     return result
+
+def get_dupe_item(bag: list) -> str:
+    return [item for item in bag[0] if item in bag[1]][0]
+
+def encode_char_to_int(char: str) -> int:
+    if ord(char) >= 97:
+        return ord(char) - 96
+    return ord(char) - 38
+
+def main():
+    load_dotenv()
+    r = requests.get("https://adventofcode.com/2022/day/3/input", cookies={"session": os.environ["COOKIE"]})
+    data = process_data(r.text)
+    print(f"part 1: {sum([encode_char_to_int(get_dupe_item(i)) for i in data])}")
+    # read from input file
+    # get the dupe for each bag
+    # encode them all
+    # sum the resulting list
+
+if __name__ == "__main__":
+    main()
--- day_3/test_day_3.py
+++ day_3/test_day_3.py
@@ -1,5 +1,7 @@
 from day_3 import (  
-    process_data
+    process_data,
+    get_dupe_item,
+    encode_char_to_int
 )
 
 sample_data = '''
@@ -11,11 +13,11 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
 '''
 
-def get_test_data(data: str) -> list:
-    return process_data(data)
+def get_test_data() -> list:
+    return process_data(sample_data)
 
 def test_process_data():
-    assert get_test_data(sample_data) == [
+    assert get_test_data() == [
         ['vJrwpWtwJgWr', 'hcsFMMfFFhFp'],
         ['jqHRNqRjqzjGDLGL', 'rsFMfFZSrLrFZsSL'],
         ['PmmdzqPrV', 'vPwwTWBwg'],
@@ -24,3 +26,11 @@ def test_process_data():
         ['CrZsJsPPZsGz', 'wwsLwLmpwMDw'],
     ]
 
+def test_get_dupe_item():
+    data = get_test_data()
+    assert get_dupe_item(data[0]) == 'p'
+
+def test_encode_char_to_int():
+    data = get_test_data()
+    char = get_dupe_item(data[0])
+    assert encode_char_to_int(char) == 16 # "p".charCode() - 96
